c931930282ec75cd9a5a7d85f6d89140e9d17b34
Marcus Brandenburger <bur@zurich.ibm.com>
2019-10-23 18:00:19 +0200

    inital auction ui mock

---
+++ examples/clock_auction/frontend/src/components/MainMenu.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-card>
+        <v-toolbar
+                color="cyan"
+                dark
+                flat
+        >
+
+            <v-toolbar-title>Participants Dashboard</v-toolbar-title>
+            <template v-slot:extension>
+                <v-tabs
+                        v-model="tab"
+                        align-with-title
+                        background-color="transparent"
+                >
+                    <v-tabs-slider color="yellow"></v-tabs-slider>
+
+                    <v-tab v-for="item in dashboard.tabs" :key="item">
+                        {{ item.text }}
+                    </v-tab>
+                </v-tabs>
+            </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+            <v-tab-item
+                    v-for="item in dashboard.tabs"
+                    :key="item"
+            >
+                <v-card flat>
+                    <v-card-text v-text="text"></v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: 'Dashboard',
+        props: ['dashboard'],
+    }
+</script>
